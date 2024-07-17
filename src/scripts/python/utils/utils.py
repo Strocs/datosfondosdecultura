9b937024e9bf45279926919ee704b76a7262257b
Strocs <strocsdev@gmail.com>
2024-07-16 22:23:57 -0400

    update: utils

--- src/scripts/python/utils/utils.py
+++ src/scripts/python/utils/utils.py
@@ -5,7 +5,7 @@ from format_id import format_id
 from get_data_from_json import get_data_from_json
 from get_file_path_from_db import get_file_path_from_db
 from get_status_and_line_from_row import get_status_and_line_from_row
-from is_data_exist_on_db import is_data_exist_on_db
+from exist_on_db import exist_on_db
 from strict_int import strict_int
 from add_obj_to_list import add_obj_to_list
 from save_data_on_json import save_data_on_json
@@ -19,7 +19,7 @@ __all__ = [
     "get_data_from_json",
     "get_file_path_from_db",
     "get_status_and_line_from_row",
-    "is_data_exist_on_db",
+    "exist_on_db",
     "strict_int",
     "add_obj_to_list",
     "save_data_on_json",
