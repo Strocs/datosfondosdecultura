9bdeee910d8d08f5cafd7be9f8a61b6aaa746e39
Strocs <strocsdev@gmail.com>
2024-07-19 00:53:28 -0400

    add: more utils

--- scripts/python/utils.py
+++ scripts/python/utils.py
@@ -1,4 +1,5 @@
 import re
+import requests
 from unidecode import unidecode
 
 
@@ -15,44 +16,6 @@ def format_id(string: str) -> str:
     return "-".join(processed_string.strip().split(" "))
 
 
-def extract_status_and_line(row: list) -> list[str]:
-    """Extract the status and project line from the given input row."""
-    content = row[0]  # Get the first element of the input list
-    normalized_content = normalize_text(content)  # Normalize the text
-
-    status_content = [
-        "Selección",
-        "Seleccionados",
-        "Lista de Espera",
-    ]  # Define the status keywords we are looking for
-
-    if status_content[2].lower() in normalized_content.lower():
-        status = status_content[2]
-    else:
-        status = status_content[0]
-
-    parts = [part.strip() for part in normalized_content.split("-")]
-
-    for part in parts:
-        if part.lower() in [status.lower() for status in status_content]:
-            parts.remove(part)
-
-    try:
-        project_line = [
-            parts[0].lower().replace(status.lower(), "").strip()
-            for status in status_content
-            if status.lower() in parts[0].lower()
-        ][0]
-    except IndexError:
-        project_line = parts[0]
-
-    if "grupo" in project_line.lower():
-        project_line = project_line.lower().split("grupo")[0].strip()
-
-    # Return the status and project line in the desired format
-    return [status, normalize_text(project_line).title()]
-
-
 def normalize_text(string: str) -> str:
     return str(string).replace("\n", " ").strip()
 
@@ -64,8 +27,57 @@ def convert_to_int(value: str) -> int | None:
         return None
 
 
-def find_id_in_data(id: int, data: list) -> bool:
-    for entry in data:
+def find_id_in_data(id: int | str, data: list) -> list[bool | int]:
+    for index, entry in data:
         if id == entry["id"]:
-            return True
-    return False
+            return [True, index]
+    return [False]
+
+
+def fetch(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.content
+
+
+def find_obj_in_list(query: str, list: list | None):
+
+    if list is None:
+        return None
+
+    for entry in list:
+        if any(
+            normalize_text(query.lower()) in str(value).lower()
+            for key, value in entry.items()
+        ):
+            return entry
+    print(f"No match was found in {list} with query: {query}")
+
+
+def get_data_by_year(year, list):
+    for item in list:
+        if item["year"] == year:
+            return item
+
+
+def add_region_to_projects(projects, regions):
+    updated_projects = []
+    for project in projects:
+        updated_projects.append(
+            {
+                **project,
+                "region": find_obj_in_list(project["region"], regions),
+            }
+        )
+    return updated_projects
+
+
+def set_new_data(new_data, db_data, update=True):
+    for data in new_data:
+        [exist, index] = find_id_in_data(data["id"], db_data)
+        if exist:
+            if update:
+                db_data[index] = data
+        else:
+            db_data.append(data)
+    return db_data
