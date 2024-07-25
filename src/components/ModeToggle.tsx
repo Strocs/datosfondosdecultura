3fb50e007b5391194f68aa3139109d26bfa41a68
Strocs <strocsdev@gmail.com>
2024-07-25 00:40:02 -0400

    style(Theme): show selected theme

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import * as React from 'react'
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
@@ -12,8 +11,25 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 
+import { cn } from '@/lib/utils'
+
+const themes = {
+  light: {
+    value: 'light',
+    label: 'Claro'
+  },
+  dark: {
+    value: 'dark',
+    label: 'Oscuro'
+  },
+  system: {
+    value: 'system',
+    label: 'Sistema'
+  }
+}
+
 export function ModeToggle () {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
 
   return (
     <DropdownMenu>
@@ -21,18 +37,33 @@ export function ModeToggle () {
         <Button variant='outline' size='icon'>
           <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
           <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-          <span className='sr-only'>Toggle theme</span>
+          <span className='sr-only'>Alternar tema</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
+        <DropdownMenuItem
+          onClick={() => setTheme(themes.light.value)}
+          className={cn({
+            'bg-accent': theme === themes.light.value
+          })}
+        >
+          {themes.light.label}
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
+        <DropdownMenuItem
+          onClick={() => setTheme(themes.dark.value)}
+          className={cn({
+            'bg-accent': theme === themes.dark.value
+          })}
+        >
+          {themes.dark.label}
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
+        <DropdownMenuItem
+          onClick={() => setTheme(themes.system.value)}
+          className={cn({
+            'bg-accent': theme === themes.system.value
+          })}
+        >
+          {themes.system.label}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
