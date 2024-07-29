e00b002563beda0b2e155433ce00cfc2378933da
Strocs <strocsdev@gmail.com>
2024-07-29 15:04:21 -0400

    feat: filters now build data from db

--- src/components/data-table/DataTableToolbar.tsx
+++ src/components/data-table/DataTableToolbar.tsx
@@ -6,38 +6,15 @@ import { Input } from '@/components/ui/input'
 import { DataTableViewOptions } from '@/components/data-table/DataTableViewOptions'
 import { DataTableFacetedFilter } from '@/components/data-table/DataTableFacetedFilter'
 import { Filter } from '@/types/projects'
-import { line, region, status, type } from '@/data/filtersData'
 
 interface DataTableToolbarProps<TData> {
   table: Table<TData>
+  filters?: Filter[]
 }
 
-// TODO: get reference data to make filters from DB on server
-const filters: Filter[] = [
-  {
-    column: 'region',
-    title: 'Región',
-    options: region
-  },
-  {
-    column: 'type',
-    title: 'Fondo',
-    options: type
-  },
-  {
-    column: 'line',
-    title: 'Línea',
-    options: line
-  },
-  {
-    column: 'status',
-    title: 'Estado',
-    options: status
-  }
-]
-
 export function DataTableToolbar<TData> ({
-  table
+  table,
+  filters
 }: DataTableToolbarProps<TData>) {
   const isFiltered = table.getState().columnFilters.length > 0
 
@@ -47,37 +24,39 @@ export function DataTableToolbar<TData> ({
         <Input
           placeholder='Buscar proyecto'
           value={
-            (table.getColumn('projectName')?.getFilterValue() as string) ?? ''
+            (table.getColumn('project_name')?.getFilterValue() as string) ?? ''
           }
           onChange={event =>
-            table.getColumn('projectName')?.setFilterValue(event.target.value)
+            table.getColumn('project_name')?.setFilterValue(event.target.value)
           }
           className='max-w-sm'
         />
-        <div className='flex flex-1 items-center gap-2 flex-wrap'>
-          {filters.map(filter => {
-            return (
-              table.getColumn(filter.column) && (
-                <DataTableFacetedFilter
-                  key={filter.column}
-                  column={table.getColumn(filter.column)}
-                  title={filter.title}
-                  options={filter.options}
-                />
+        {filters && (
+          <div className='flex flex-1 items-center gap-2 flex-wrap'>
+            {filters.map(filter => {
+              return (
+                table.getColumn(filter.column) && (
+                  <DataTableFacetedFilter
+                    key={filter.column}
+                    column={table.getColumn(filter.column)}
+                    title={filter.title}
+                    options={filter.options}
+                  />
+                )
               )
-            )
-          })}
-          {isFiltered && (
-            <Button
-              variant='ghost'
-              onClick={() => table.resetColumnFilters()}
-              className='h-8 px-2 lg:px-3'
-            >
-              Reset
-              <Cross2Icon className='ml-2 h-4 w-4' />
-            </Button>
-          )}
-        </div>
+            })}
+            {isFiltered && (
+              <Button
+                variant='ghost'
+                onClick={() => table.resetColumnFilters()}
+                className='h-8 px-2 lg:px-3'
+              >
+                Reset
+                <Cross2Icon className='ml-2 h-4 w-4' />
+              </Button>
+            )}
+          </div>
+        )}
       </div>
       <DataTableViewOptions table={table} />
     </div>
