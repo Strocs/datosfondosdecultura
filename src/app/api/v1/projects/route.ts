459f3941cef9b24b77aed864a70636e080c01896
Strocs <strocsdev@gmail.com>
2024-08-01 12:17:35 -0400

    api: connect to remote db

--- src/app/api/v1/projects/route.ts
+++ src/app/api/v1/projects/route.ts
@@ -2,26 +2,74 @@ import { getProjects } from '@/services/projectService';
 import { Project, APIResponse } from '@/types/projects';
 import { getPaginatedData } from '@/utils/api/getPaginatedData';
 import { parseQueryParams } from '@/utils/api/parseQueryParams';
+import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
+
+
+let cachedClient: PrismaClient<{ adapter: PrismaLibSQL }> | null = null
 
 export async function GET(request: Request) {
+  if (!cachedClient) {
+    const client = createClient({
+      url: `${process.env.TURSO_DATABASE_URL}`,
+      authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+    })
+    const adapter = new PrismaLibSQL(client)
+    cachedClient = new PrismaClient({ adapter })
+  }
+
+  const prisma = cachedClient
+
   try {
     const { searchParams, origin, pathname } = new URL(request.url);
 
     const { region, line, type, sortBy, order, page, limit } = parseQueryParams(searchParams)
 
 
-    if (region) {
-      // TODO: paginate data if have more than 20 projects
-      const { projects, length, region: matchedRegion } = await getProjects({ region })
-      return new Response(JSON.stringify({ length, region: matchedRegion?.region_name, projects }), { status: 200, headers: { 'Content-Type': 'application/json' } })
-    }
+    // if (region) {
+    //   // TODO: paginate data if have more than 20 projects
+    //   const { projects, length, region: matchedRegion } = await getProjects({ region })
+    //   return new Response(JSON.stringify({ length, region: matchedRegion?.region_name, projects }), { status: 200, headers: { 'Content-Type': 'application/json' } })
+    // }
 
-    if (page < 1 || limit < 1) return new Response('Invalid query parameters', { status: 400 });
+    // if (page < 1 || limit < 1) return new Response('Invalid query parameters', { status: 400 });
 
 
-    const { projects, length } = await getProjects();
+    // const { projects, length } = await getProjects();
+
+
+    const projects = await prisma.projects.findMany({
+      select: {
+        project_id: true,
+        project_name: true,
+        project_owner: true,
+        region: {
+          select: {
+            region_name: true
+          }
+        },
+        fund: {
+          select: {
+            fund_name: true
+          }
+        },
+        project_year: true,
+        line: {
+          select: {
+            line_name: true
+          }
+        },
+        modality: true,
+        status: true,
+        amount_assigned: true,
+      }
+    })
+
     let paginatedData = getPaginatedData<Project>(projects, page, limit)
+
+    const length = projects.length
 
     const next_url = `${origin + pathname}?page=${page + 1}&limit=${limit}`
     const prev_url = `${origin + pathname}?page=${page - 1}&limit=${limit}`
