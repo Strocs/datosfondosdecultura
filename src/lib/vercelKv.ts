57b25983527a1efe8277124c902964b3a582f8ed
Strocs <strocsdev@gmail.com>
2024-08-02 00:06:43 -0400

    fix: env names on kv config

--- src/lib/vercelKv.ts
+++ src/lib/vercelKv.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@vercel/kv';
 
 const kv = createClient({
-  url: process.env.VERCEL_KV_URL,
-  token: process.env.VERCEL_KV_TOKEN,
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
 });
 
 export default kv;
