107aa4217a03cedf936d32d20a152e1e0fc4c853
Strocs <strocsdev@gmail.com>
2025-11-08 01:27:58 -0300

    refactor: update projects page to use new db and remove old filters

--- src/app/(projects)/page.tsx
+++ src/app/(projects)/page.tsx
@@ -1,7 +1,5 @@
 import { DataTable } from '@/components/data-table/data-table'
 import { columns } from './columns'
-import { filters } from './filters'
-import { getProjects } from '@/services/projectService'
 import {
   Card,
   CardContent,
@@ -9,18 +7,15 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-import { formatAmount } from '@/utils/formatAmount'
 import { Chart } from './chart'
 import { Chile } from '@/components/Chile'
 import { Header } from '@/components/Header'
+import { getByQuery } from '@/lib/db'
 
 export default async function ProjectsPage() {
-  const { projects: data } = await getProjects()
+  const { rows: data } = await getByQuery('SELECT * FROM projects')
 
   const year = data[0].project_year
-  const totalAmmount = data.reduce((acc, curr) => {
-    return acc! + curr.amount_assigned!
-  }, data[0].amount_assigned)
 
   return (
     <>
@@ -29,7 +24,7 @@ export default async function ProjectsPage() {
         {/* SIDEBAR */}
         {/* <aside className='sticky top-4 h-[calc(100dvh-2rem)] w-full max-w-xs rounded-xl border bg-background'></aside> */}
 
-        <div className='fixed right-0 top-0 z-10 py-[--padding-y] pr-20 [--padding-y:1rem]'>
+        <div className='fixed top-0 right-0 z-10 py-[--padding-y] pr-20 [--padding-y:1rem]'>
           <Chile className='h-[calc(100vh-(var(--padding-y)*2))]' />
         </div>
         <div className='w-9/12 p-4'>
@@ -41,16 +36,14 @@ export default async function ProjectsPage() {
                 <CardDescription>Para proyectos seleccionados</CardDescription>
               </CardHeader>
               <CardContent>
-                <p className='text-xl font-bold'>
-                  {formatAmount(totalAmmount!)}
-                </p>
+                <p className='text-xl font-bold'>'mucho'</p>
               </CardContent>
             </Card>
             <Chart />
           </section>
 
           {/* DATA TABLE */}
-          <DataTable columns={columns} data={data} filters={filters} />
+          <DataTable columns={columns} data={data} />
         </div>
       </main>
       {/* <Footer /> */}
